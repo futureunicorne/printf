b6c15178b86625189384f2fdf717c38c1bf0f731
futureunicorne <elhadi-hamza@hotmail.fr>
2017-02-06 12:56:40 +0100

    printf1

--- src/ft_putbin/ft_putbin_f.c
+++ src/ft_putbin/ft_putbin_f.c
@@ -6,30 +6,53 @@
 /*   By: hel-hadi <hel-hadi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/05 23:56:28 by hel-hadi          #+#    #+#             */
-/*   Updated: 2017/02/06 00:00:10 by hel-hadi         ###   ########.fr       */
+/*   Updated: 2017/02/06 12:43:28 by hel-hadi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int	ft_puthexa_f(char *s, va_list *p)
+int		ft_nbr_bin(unsigned long long nb)
+{
+	int i;
+
+	i = 0;
+	while (nb > 0)
+	{
+
+	}
+}
+
+void	ft_puthexa(unsigned nb)
+{
+	char *tab = "0111111111";
+	if (nb < 2)
+		ft_putchar(tab[nb]);
+	if (nb > 1)
+	{
+		ft_puthexa(nb / 2);
+		ft_putchar(tab[nb % 2]);
+	}
+}
+
+void	ft_putbin(unsigned long long nb)
+{
+	char *tab = "0123456789abcdef";
+	if (nb <= 15)
+		ft_putchar(tab[nb]);
+	if (nb >= 16)
+	{
+		ft_putbin(nb / 16);
+		ft_putchar(tab[nb % 16]);
+	}
+}
+
+int	ft_putbin_f(char *s, va_list *p)
 {
    unsigned		arg;
    t_ptr 		ptr;
 
    ft_memset(&ptr, 0, sizeof(t_ptr));
-   if (ft_check_long(s))
-   {
-	   if (ft_check_long(s) == 'l')
-		   ptr.val = ft_putbin_l(s, p);
-	   else if (ft_check_long(s) == 'h')
-		   ptr.val = ft_putbin_h(s, p);
-	   else if (ft_check_long(s) == 'j')
-		   ptr.val = ft_putbin_j(s, p);
-	   else if (ft_check_long(s) == 'z')
-		   ptr.val = ft_putbin_z(s, p);
-	   return (ptr.val);
-   }
    ptr.check_type = 1;
    arg = va_arg(*p, unsigned);
    ptr.nb = ft_nbr_bin((unsigned)arg);
@@ -39,3 +62,4 @@ int	ft_puthexa_f(char *s, va_list *p)
    ptr.val = ptr.val + ft_size_chain_bin_plus(s, ptr.nb, ptr.diff);
    return (ptr.val + ptr.nb);
 }
+
--- src/ft_putbin/ft_size_chain_bin.c
+++ src/ft_putbin/ft_size_chain_bin.c
@@ -6,8 +6,83 @@
 /*   By: hel-hadi <hel-hadi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/05 23:56:46 by hel-hadi          #+#    #+#             */
-/*   Updated: 2017/02/05 23:56:56 by hel-hadi         ###   ########.fr       */
+/*   Updated: 2017/02/06 12:43:33 by hel-hadi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+
+int	ft_size_chain_bin_bis(t_siz *siz, char *s, int t_arg, int diff)
+{
+	if (ft_check_zero(s) == 1)
+		siz->d = '0';
+	if (ft_check_zero(s) == 0)
+		siz->d = ' ';
+	if (!siz->nbr_prec && siz->nbr)
+		siz->len = siz->nbr - t_arg - diff;
+	else if (!siz->nbr && siz->nbr_prec)
+	{
+		siz->len = siz->nbr_prec - t_arg;
+		siz->d = '0';
+	}
+	else if (siz->nbr && siz->nbr_prec)
+	{
+		if (siz->nbr == siz->nbr_prec)
+			siz->len = siz->nbr - t_arg;
+		else if (siz->nbr < siz->nbr_prec)
+			siz->len = siz->nbr_prec - t_arg;
+		siz->ecart =  siz->nbr - siz->nbr_prec -  diff;
+		siz->d = '0';
+		siz->e = ' ';
+	}
+	if (siz->ecart && (!siz->len))
+		siz->len = siz->ecart;
+	return (0);
+}
+
+int	ft_size_chain_bin(char *s, int t_arg, int diff)
+{
+	t_siz siz;
+
+	ft_memset(&siz, 0, sizeof(t_siz));
+	siz.nbr = ft_record_chain(s);
+	siz.nbr_prec = ft_record_prec(s);
+	if (ft_check_less(s) && siz.nbr_prec <= t_arg)
+		return (siz.val);
+	siz.len = siz.nbr - t_arg - diff;
+	ft_size_chain_bin_bis(&siz, s, t_arg, diff);
+	while (siz.i < siz.len)
+	{
+		if (siz.i < siz.ecart)
+		{
+			ft_putchar(siz.e);
+			siz.val++;
+		}
+		else if (siz.i >= siz.ecart)
+		{
+			siz .val++;
+			ft_putchar(siz.d);
+		}
+		siz.i++;
+	}
+	return (siz.val);
+}
+
+int	ft_size_chain_bin_plus(char *s, int t_arg, int diff)
+{
+	t_siz siz;
+
+	ft_memset(&siz, 0, sizeof(t_siz));
+	siz.nbr = ft_record_chain(s);
+	siz.nbr_prec = ft_record_prec(s);
+	if (ft_check_less(s) == 0)
+		return (siz.val);
+	siz.len = siz.nbr - t_arg - diff;
+	while (siz.i < siz.len)
+	{
+		ft_putchar(' ');
+		siz.i++;
+		siz.val++;
+	}
+	return (siz.val);
+}
