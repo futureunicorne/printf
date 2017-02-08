8f9c735fa3d8ec108be4a79a680e2fc2ad8c4648
futureunicorne <elhadi-hamza@hotmail.fr>
2017-02-08 14:58:36 +0100

    printf1

---
+++ main.c
@@ -0,0 +1,68 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hel-hadi <hel-hadi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/01/09 11:27:15 by hel-hadi          #+#    #+#             */
+/*   Updated: 2017/02/08 13:52:29 by hel-hadi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "src/ft_printf.h"
+#include "src/libft/libft.h"
+
+int main(void)
+{
+  ft_printf("\n");
+  ft_printf("%%\n");
+  ft_printf("%d\n", 42);
+  ft_printf("%d%d\n", 42, 41);
+  ft_printf("%d%d%d\n", 42, 43, 44);
+  ft_printf("%ld\n", 2147483647);
+  ft_printf("%lld\n", 9223372036854775807);
+  ft_printf("%x\n", 505);
+  ft_printf("%X\n", 505);
+  ft_printf("%p\n", &ft_printf);
+  ft_printf("%20.15d\n", 54321);
+  ft_printf("%-10d\n", 3);
+  ft_printf("% d\n", 3);
+  ft_printf("%+d\n", 3);
+  ft_printf("%010d\n", 1);
+  ft_printf("%hhd\n", 0);
+  ft_printf("%jd\n", 9223372036854775807);
+  ft_printf("%zd\n", 4294967295);
+  ft_printf("%\n");
+  ft_printf("%U\n", 4294967295);
+  ft_printf("%u\n", 4294967295);
+  ft_printf("%o\n", 40);
+  ft_printf("%%#08x\n", 42);
+  ft_printf("%x\n", 1000);
+  ft_printf("%#X\n", 1000);
+  ft_printf("%s\n", NULL);
+  ft_printf("%S\n", L"ݗݜशব");
+  ft_printf("%s%s\n", "test", "test");
+  ft_printf("%s%s%s\n", "test", "test", "test");
+  ft_printf("%C\n", 15000);
+  return (0);
+}
+
+
+//d ok
+//x nok
+
+/*
+// type x est un unsigned
+pour chaine de caracatere cela ninflue que sur la taille d e la chaine,
+le reste des des flag  reste inchanges
+gerer % + d +%
+
+#ifndef BUILTIN
+	(void) setlocale(LC_NUMERIC, "");
+
+pour le numeriques, rajoute 0 pour completer la precision demandes, si la largeur de champs
+est preciser et que l
+
+printf("oui\n");
+*/
